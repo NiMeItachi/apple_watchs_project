c2164d156c8f00895036c502455d8d2c72e80aa2
Victus <nursultan.suleymanov.2005@gmail.com>
2023-05-11 08:59:01 +0500

    exam5

---
+++ android/app/src/main/kotlin/com/example/b28_nursultan_suleymanov_exam5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.b28_nursultan_suleymanov_exam5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:b28_nursultan_suleymanov_exam5/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
